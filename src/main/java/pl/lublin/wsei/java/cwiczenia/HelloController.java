0008e56daeae16484399d48aac6d283f896a1adf
Bartek <bartek.burek@onet.pl>
2022-05-04 19:40:01 +0200

    Lab 2b Punkt 3 e-f Dodanie obsługi zdarzeń i deklaracji oraz definicji funkcji w pliku HelloController

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,14 +1,15 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label IbFile;
+
+    public void btnOpenFileAction(ActionEvent actionEvent)
+    {
+
     }
 }
