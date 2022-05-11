3c4984088a3a1f2148259cc0fd45ab5a3e6325de
Bartek <bartek.burek@onet.pl>
2022-05-11 18:39:07 +0200

    Lab 2b Punkt 7 Dodanie otwierania grafiki w nowym oknie (nie działa)

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -7,6 +7,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -17,6 +20,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class HelloController {
 
@@ -97,5 +101,31 @@ public class HelloController {
     public void setHostServices (HostServices hostServices)
     {
         this.hostServices = hostServices;
+    }
+
+    public void btnPokaz(ActionEvent actionEvent)
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("imgViewer.fxml"));
+            Parent root = loader.load();
+            ImgViewer viewer = loader.getController();
+            if(selInfografika != null)
+            {
+                Image img = new Image(selInfografika.adresGrafiki);
+                viewer.imgView.setFitWidth(img.getWidth());
+                viewer.imgView.setFitHeight(img.getHeight());
+                viewer.imgView.setImage(img);
+            }
+
+            Stage stage = new Stage();
+            stage.setTitle("Podgląd Infografiki");
+            stage.setScene(new Scene(root, 900, 1800));
+            stage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
